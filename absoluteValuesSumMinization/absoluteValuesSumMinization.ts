683b2c89606310d43cc943ed1cc01b645586e8f4
Jesse Spurlin <jessespurlin@gmail.com>
2019-07-25 23:41:46 -0500

    finished

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
 function absoluteValuesSumMinimization(a: number[]): number {
+  // abs(a[0] - x) + abs(a[1] - x) + ... + abs(a[a.length - 1] - x)
+  const isEven = a.length % 2 === 0;
 
+  return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,24 @@
-function addTwoDigits(n: any): number {
+// function addTwoDigits(n: any): number {
+//   let sum = 0;
+  
+//   n.toString().split('');
+//   n.forEach( num => {
+//     sum += parseInt(num);
+//   })
 
-}
+//   return sum;
+// }
 
 // console.log(addTwoDigits(29));
+
+
+function addTwoDigits(n: any): number {
+  const nums = n.toString().split('');
+  let sum = 0;
+
+  nums.forEach( num => sum += parseInt(num) )
+
+  return sum;
+}
+
+console.log(addTwoDigits(29));
