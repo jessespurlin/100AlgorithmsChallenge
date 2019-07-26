efe66ff67f4b4e75e228fd201d84f03960542036
Jesse Spurlin <jessespurlin@gmail.com>
2019-07-26 00:00:00 -0500

    finished

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,19 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let maxLength = 0;
+  let longestStrings = [];
 
+  for(let i = 0; i < inputArray.length; i++){
+    if(inputArray[i].length > maxLength){
+      maxLength = inputArray[i].length;
+    }
+  }
+  inputArray.forEach( e => {
+    if(e.length === maxLength){
+      longestStrings.push(e);
+    }
+  })
+  return longestStrings;
 }
 
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
